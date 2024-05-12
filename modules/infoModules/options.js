d1ec70ad0e5886c97a16176ec3fac2b710de2489
Erbol <erb0_01@mail.ru>
2024-05-12 23:53:35 +0500

    add payment and cheap func

--- modules/infoModules/options.js
+++ modules/infoModules/options.js
@@ -1,13 +1,16 @@
-const { Markup } = require("telegraf");
+const { Markup } = require('telegraf')
 
 module.exports = {
-  menu: Markup.keyboard([
-    [{ text: "💸 платежи" }, { text: "📋 показания" }],
-    [{ text: "🔍 поиск" }],
-  ]).resize(),
-  reportKb: Markup.keyboard([[{ text: "повторить поиск" }]]).resize(),
+  menu: Markup.inlineKeyboard([
+    [
+      Markup.button.callback('💸 платежи', 'payments'),
+      Markup.button.callback('📋 показания', 'cheap'),
+    ],
+    [Markup.button.callback('🔍 поиск', 'search')],
+  ]),
+  reportKb: Markup.keyboard([[{ text: 'повторить поиск' }]]).resize(),
   search: Markup.keyboard([
-    [{ text: "🔍 по л/с" }, { text: "🔍 по вм" }],
-    [{ text: "🔍 по фио" }],
+    [{ text: '🔍 по л/с' }, { text: '🔍 по вм' }],
+    [{ text: '🔍 по фио' }],
   ]).resize(),
-};
+}
--- modules/infoModules/sqlQuery.js
+++ modules/infoModules/sqlQuery.js
@@ -1,54 +1,60 @@
-const { connection, checkConnection, userState } = require("../accessDb");
-const { deskCodes, paymentCodes, streetCodes } = require("./dataObjects");
-const { menu } = require("./options");
+const { connection, checkConnection, userState } = require('../accessDb')
+const { deskCodes, paymentCodes, streetCodes } = require('./dataObjects')
+const { menu } = require('./options')
 
 async function searchByUser(text, ctx, User) {
-  const chatId = ctx.from.id;
-  userState[chatId] = { searchValue: parseInt(text) };
-  const user = await User.findOne({ user_id: chatId });
+  const chatId = ctx.from.id
+  userState[chatId] = { searchValue: parseInt(text) }
+  const user = await User.findOne({ user_id: chatId })
 
   const sqlSearch = `
   SELECT CONSUM.CONSNAME, CONSUM.STRTCODE, CONSUM.HOUSE, зTOTPAY_ALL_Тек.Долг, зTOTPAY_ALL_Тек.ДатаРсч
   FROM CONSUM INNER JOIN зTOTPAY_ALL_Тек ON CONSUM.CONSCODE = зTOTPAY_ALL_Тек.CONSCODE
-  WHERE CONSUM.CONSCODE = ${userState[chatId].searchValue};`;
+  WHERE CONSUM.CONSCODE = ${userState[chatId].searchValue};`
 
   try {
-    await checkConnection();
-    const userData = await connection.query(sqlSearch);
+    await checkConnection()
+    const userData = await connection.query(sqlSearch)
     if (userData && userData.length > 0) {
-      const streetCode = userData[0].STRTCODE;
-      const streetName = streetCodes[streetCode];
-      const dataRep = userData[0].ДатаРсч.slice(0, 10);
+      const streetCode = userData[0].STRTCODE
+      const streetName = streetCodes[streetCode]
+      const dataRep = userData[0].ДатаРсч.slice(0, 10)
       const userProfile = `👤 ( ${userState[chatId].searchValue} ) ${
         userData[0].CONSNAME
+      }\n🏠 адрес:${streetName} ${
+        userData[0].HOUSE
+      }\n💰 долг: ${userData[0].Долг.toFixed(
+        0
+      )} тг\n🧾 дата расчета: ${dataRep}`
+
+      const sentMessage = await ctx.replyWithHTML(userProfile, menu)
+      user.data = {
+        ...user.data,
+        searchValue: userState[chatId].searchValue,
+        sentMessage: sentMessage.message_id,
       }
-    🏠 адрес:${streetName} ${userData[0].HOUSE}
-    💰 долг: ${userData[0].Долг.toFixed(0)} тг
-    🧾 дата расчета: ${dataRep}`;
-      ctx.replyWithHTML(userProfile, menu);
-      user.data = { ...user.data, searchValue: userState[chatId].searchValue };
-      await user.save();
+      await user.save()
     } else {
-      userState[chatId] = { state: "search" };
-      const message = `Нет результатов для л/с ${userState[chatId].searchValue}`;
-      ctx.reply(message);
+      userState[chatId] = { state: 'search' }
+      const message = `Нет результатов для л/с ${userState[chatId].searchValue}`
+      ctx.reply(message)
     }
   } catch (error) {
-    console.error("Ошибка при выполнении запроса:", error);
+    console.error('Ошибка при выполнении запроса:', error)
   }
 }
 
 async function searchByWm(chatId, text, bot) {
-  const searchValue = parseInt(text);
+  const searchValue = parseInt(text)
 
-  const byWmQuery = `SELECT * FROM з_АбонентыВМ WHERE [wm] LIKE '%${searchValue}%'`;
+  const byWmQuery = `SELECT * FROM з_АбонентыВМ WHERE [wm] LIKE '%${searchValue}%'`
 
   try {
-    await checkConnection();
-    const wmData = await connection.query(byWmQuery);
+    await checkConnection()
+    const wmData = await connection.query(byWmQuery)
     if (wmData && wmData.length > 0) {
       for (let i = 0; i < wmData.length; i++) {
-        const userProfileWm = `Л/с: ${wmData[i].userId}\nАбонент: ${wmData[i].user}\nУчасток: ${wmData[i].location}\nВодомер: ${wmData[i].wm}`;
+        const userProfileWm = `Л/с: ${wmData[i].userId}\nАбонент: ${wmData[i].user}\nУчасток: ${wmData[i].location}\nВодомер: ${wmData[i].wm}`
 
         const inlineKeyboard = [
           [
@@ -57,34 +63,34 @@ async function searchByWm(chatId, text, bot) {
               callback_data: `searchUser_${wmData[i].userId}`,
             },
           ],
-        ];
+        ]
 
         const options = {
           reply_markup: {
             inline_keyboard: inlineKeyboard,
           },
-        };
-        await bot.sendMessage(chatId, userProfileWm, options);
+        }
+        await bot.sendMessage(chatId, userProfileWm, options)
       }
     } else {
-      const message = `Нет результатов для в/м ${searchValue}`;
-      bot.sendMessage(chatId, message);
+      const message = `Нет результатов для в/м ${searchValue}`
+      bot.sendMessage(chatId, message)
     }
   } catch (error) {
-    console.error("Ошибка при выполнении запроса:", error);
+    console.error('Ошибка при выполнении запроса:', error)
   }
 }
 
 async function searchByName(chatId, text, bot) {
-  const searchValue = text;
-  const byWmQuery = `SELECT * FROM з_АбонентыВМ WHERE [name] LIKE '%${searchValue}%'`;
+  const searchValue = text
+  const byWmQuery = `SELECT * FROM з_АбонентыВМ WHERE [name] LIKE '%${searchValue}%'`
 
   try {
-    await checkConnection();
-    const wmData = await connection.query(byWmQuery);
+    await checkConnection()
+    const wmData = await connection.query(byWmQuery)
     if (wmData && wmData.length > 0) {
       for (let i = 0; i < wmData.length; i++) {
-        const userProfileWm = `Л/с: ${wmData[i].userId}\nАбонент: ${wmData[i].user}\nУчасток: ${wmData[i].location}\nВодомер: ${wmData[i].wm}`;
+        const userProfileWm = `Л/с: ${wmData[i].userId}\nАбонент: ${wmData[i].user}\nУчасток: ${wmData[i].location}\nВодомер: ${wmData[i].wm}`
         const inlineKeyboard = [
           [
             {
@@ -92,102 +98,121 @@ async function searchByName(chatId, text, bot) {
               callback_data: `searchUser_${wmData[i].userId}`,
             },
           ],
-        ];
+        ]
         const options = {
           reply_markup: {
             inline_keyboard: inlineKeyboard,
           },
-        };
-        await bot.sendMessage(chatId, userProfileWm, options);
+        }
+        await bot.sendMessage(chatId, userProfileWm, options)
       }
     } else {
-      const message = `Нет результатов для фио ${searchValue}`;
-      bot.sendMessage(chatId, message);
+      const message = `Нет результатов для фио ${searchValue}`
+      bot.sendMessage(chatId, message)
     }
   } catch (error) {
-    console.error("Ошибка при выполнении запроса:", error);
+    console.error('Ошибка при выполнении запроса:', error)
   }
 }
 
-async function searchPayment(chatId, userState, bot) {
-  // let table = `Л/счет - ${userState[chatId].searchValue}\n\nСписок последних оплат:
-  //   \n📅 Дата 💳 Касса 📑 Вид.опл 💰 Сумма\n`;
-  let table = `Список последних оплат:
-    \n📅 Дата 💳 Касса 📑 Вид.опл 💰 Сумма\n`;
+async function searchPayment(User, ctx) {
+  const chatId = ctx.from.id
+  const user = await User.findOne({ user_id: chatId })
+
+  let table = `👤 ( ${user.data.searchValue} )\nСписок последних оплат:
+    \n📅 Дата 💳 Касса 📑 Вид.опл 💰 Сумма\n`
 
   const sqlQuery = `SELECT TOP 12 SUMMA, PDESKCODE, PDATE, GROUPCODE
-    FROM HEAP WHERE CONSCODE = ${userState[chatId].searchValue}
-    ORDER BY PDATE DESC`;
+    FROM HEAP WHERE CONSCODE = ${user.data.searchValue}
+    ORDER BY PDATE DESC`
 
   try {
-    await checkConnection();
+    await checkConnection()
 
-    const data = await connection.query(sqlQuery);
+    const data = await connection.query(sqlQuery)
 
     if (data && data.length > 0) {
       for (let i = 0; i < data.length; i++) {
-        const deskCode = data[i].PDESKCODE;
-        const deskName = deskCodes[deskCode] || "";
-        const paymentCode = data[i].GROUPCODE;
-        const paymentName = paymentCodes[paymentCode] || "";
-        const dateOnly = data[i].PDATE.substring(0, 10);
+        const deskCode = data[i].PDESKCODE
+        const deskName = deskCodes[deskCode] || ''
+        const paymentCode = data[i].GROUPCODE
+        const paymentName = paymentCodes[paymentCode] || ''
+        const dateOnly = data[i].PDATE.substring(0, 10)
         table += `${dateOnly}, ${deskName}, ${paymentName}, ${data[
           i
-        ].SUMMA.toFixed(2)} тг\n`;
+        ].SUMMA.toFixed(2)} тг\n`
       }
 
-      bot.sendMessage(chatId, table, menu);
+      await ctx.telegram.editMessageText(
+        chatId, // Идентификатор чата, куда было отправлено сообщение
+        user.data.sentMessage, // Идентификатор изменяемого сообщения
+        null, // Если вы не хотите менять местоположение сообщения (по умолчанию null)
+        table,
+        menu // Новый текст сообщения
+      )
     } else {
-      const message = `Нет результатов для л/с ${userState[chatId].searchValue}`;
-      bot
-        .sendMessage(chatId, message)
-        .catch((error) =>
-          console.error("Ошибка при отправке сообщения:", error)
-        );
+      const message = `Нет результатов для л/с ${user.data.searchValue}`
+      ctx.reply(message)
     }
   } catch (error) {
-    console.error("Ошибка при выполнении запроса:", error);
+    console.error('Ошибка при выполнении запроса:', error)
   }
 }
 
-async function searchCheap(chatId, userState, bot) {
+async function searchCheap(User, ctx) {
+  const chatId = ctx.from.id
+  const user = await User.findOne({ user_id: chatId })
+
+  try {
+    const waterMeters = await getWaterMeters(user.data.searchValue)
+    const formattedResult = await formatResult(waterMeters)
+
+    ctx.replyWithHTML(formattedResult, menu) // Отправляем сообщение
+  } catch (error) {
+    console.error('Ошибка при выполнении запроса:', error)
+  }
+}
+
+async function searchCheap(User, ctx) {
+  const chatId = ctx.from.id
+  const user = await User.findOne({ user_id: chatId })
   try {
     const searchQuery = `
         SELECT DISTINCT WCOUNT.WCODE, WCOUNT.FACTNUMB, WCOUNT.DATESET
         FROM WCOUNT
-        WHERE WCOUNT.CONSCODE = ${userState[chatId].searchValue}
-      `;
+        WHERE WCOUNT.CONSCODE = ${user.data.searchValue}
+      `
 
-    const waterMeters = await connection.query(searchQuery);
+    const waterMeters = await connection.query(searchQuery)
 
-    let formattedResult = "";
+    let formattedResult = ''
     for (const waterMeter of waterMeters) {
-      const { WCODE, FACTNUMB, DATESET } = waterMeter;
-      const sliceDateset = DATESET.slice(0, 10);
+      const { WCODE, FACTNUMB, DATESET } = waterMeter
+      const sliceDateset = DATESET.slice(0, 10)
       formattedResult += `Номер водомера: ${FACTNUMB}\nДата установки: ${sliceDateset}
-        \n📅 Дата | 📋Показания\n`;
+        \n📅 Дата | 📋Показания\n`
 
       const waterMeterQuery = `
           SELECT TOP 12 WCHEAP.CURRCOUNT, WCHEAP.LASTDATE
           FROM WCHEAP
           WHERE WCHEAP.WCODE = ${WCODE}
           ORDER BY WCHEAP.LASTDATE DESC
-        `;
+        `
 
-      const result = await connection.query(waterMeterQuery);
+      const result = await connection.query(waterMeterQuery)
 
       for (const row of result) {
-        const { CURRCOUNT, LASTDATE } = row;
-        const sliceDate = LASTDATE.slice(0, 10);
-        formattedResult += `${sliceDate} | ${CURRCOUNT}\n`;
+        const { CURRCOUNT, LASTDATE } = row
+        const sliceDate = LASTDATE.slice(0, 10)
+        formattedResult += `${sliceDate} | ${CURRCOUNT}\n`
       }
 
-      formattedResult += "------------------------\n";
+      formattedResult += '------------------------\n'
     }
 
-    bot.sendMessage(chatId, formattedResult, menu); // Отправляем сообщение
+    ctx.replyWithHTML(formattedResult, menu) // Отправляем сообщение
   } catch (error) {
-    console.error("Ошибка при выполнении запроса:", error);
+    console.error('Ошибка при выполнении запроса:', error)
   }
 }
 
@@ -197,4 +222,4 @@ module.exports = {
   searchCheap,
   searchByUser,
   searchByWm,
-};
+}
